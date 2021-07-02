e15f3b3db2a81a4b78c9d0c5630be5d204d57b45
Valentain1993 <chernikovave1993@gmail.com>
2021-07-02 15:55:42 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log(1);
+
+let number = 5;
+let noNumber = '56';
+let green = 'Hello';
+
+console.log(number / noNumber); // почему в консоли выводит результат, словно "56" это число, а не строка?
+
+//alert(`В слове "привет" ${number} букв!`)
+
+let amper1 = false,
+    amper2 = false;
+
+alert(amper1 && amper2);
